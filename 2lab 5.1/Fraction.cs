b4093b6388e4789460960ffd7c9d216d1977e4ad
Yodelyoda <47818799+Yodelyoda@users.noreply.github.com>
2022-04-19 23:54:34 +0300

    Add files via upload

---
+++ 2lab 5.1/Fraction.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2lab_5._1
+{
+    class Fraction
+    {
+        private long c;
+        private int d;
+        public Fraction()
+        {
+            c = 0;
+            d = 0;
+        }
+        public Fraction(long c, int d)
+        {
+            this.c = c;
+            this.d = d;
+        }
+        public Fraction(Fraction c)
+        {
+            this.c = c.c;
+            this.d = c.d;
+        }
+        public void SetC(long c)
+        {
+            this.c = c;
+        }
+        public void SetD(int d)
+        {
+            this.d = d;
+        }
+        public long GetC()
+        {
+            return this.c;
+        }
+        public int GetD()
+        {
+            return this.d;
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return new Fraction(a.c + b.c, a.d + b.d);
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return new Fraction(a.c / b.c, a.d / b.d);
+        }
+        public static Fraction operator %(Fraction a, Fraction b)
+        {
+            return new Fraction(a.c % b.c, a.d % b.d);
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            long c1 = a.c;
+            int d1 = a.d;
+           long c2 = b.c;
+            int d2 = b.d;
+        
+            return new Fraction(c1/2,d1/2);
+        }
+        public static Fraction operator ^(Fraction a, UInt32 n)
+        {
+            
+            var y = new Fraction(1, 0);
+            for (UInt32 i = 1; i <= n; i++)
+             y = y * a;
+            return y;
+        }
+        
+        public override string ToString()
+        {
+            return " long: " + this.c + "\n shor: " + this.d;
+        }
+    }
+}
---
+++ 2lab 5.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2lab_5._1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var a = new Fraction(1, 1);
+            var b = new Fraction(1, 1);
+            var g = new Fraction(1, 1);
+            Console.WriteLine(a);
+
+            Console.WriteLine("cila chastina: ");
+            long c = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("drova chastina: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Fraction x = new Fraction(c, d);
+            Fraction y = new Fraction();
+            Console.WriteLine("vidnimania: ");
+            y = x - b;
+            String y1 = y.ToString();
+            Console.WriteLine("Result: x-b =", y1);
+            Console.WriteLine("parnist i ostaca: ");
+            y = x % b;
+            String y2 = y.ToString();
+            Console.WriteLine("Result: x % b =", y2);
+            if (y != a)
+            {
+                Console.WriteLine("Result: x i b parni");
+            }
+            else
+            {
+                Console.WriteLine("Result: x i b ne parni");
+            }
+            Console.WriteLine("dilenia: ");
+            y = x / b;
+            String y3 = y.ToString();
+            Console.WriteLine("Result: x/b =", y3);
+            Console.WriteLine("Pidnesenia: ");
+            y = x ^ 2;
+            String y4 = y.ToString();
+            Console.WriteLine("Result: x^b =", y4);
+            Console.ReadKey();
+        }
+    }
+}
